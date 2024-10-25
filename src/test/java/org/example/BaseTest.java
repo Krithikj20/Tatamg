c8716dc749b42fc7a55132dbf5f884063d8aba36
krithikj20 <krithik@geekyants.com>
2024-10-25 11:46:00 +0530

    added BaseTest

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseTest {
+
+   public static WebDriver driver;
+
+    public void setup(){
+       driver=new ChromeDriver();
+       driver.get("https://www.1mg.com/");
+       driver.manage().window().maximize();
+
+    }
+
+    public void tearDown(){
+//driver.quit();
+    }
 }
